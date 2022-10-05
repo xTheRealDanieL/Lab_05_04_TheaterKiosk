fff3e0a94670dd75654de3cdc1c05ee64f40ffd7
Long <nguye3lg@mail.uc.edu>
2022-10-05 16:17:50 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int age;
+        age = 17;
+
+        if (age>21)
+            System.out.println("you can enter the theatre!");
+
+        else
+            System.out.println();
+
+
+    }
+}
